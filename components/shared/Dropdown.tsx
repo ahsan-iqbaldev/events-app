adba79d042070525f7784f67c74445e9c9192045
Ahsan Iqbal <revensolutions01@gmail.com>
2024-06-02 02:15:40 +0500

    create title and dropdown field in event form

---
+++ components/shared/Dropdown.tsx
@@ -0,0 +1,84 @@
+import React, { startTransition, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Input } from "../ui/input";
+
+type DropDownProps = {
+  value?: string;
+  onChangeHandler?: () => void;
+};
+
+const Dropdown = ({ value, onChangeHandler }: DropDownProps) => {
+  const [category, setCategory] = useState([
+    { id: "1", name: "Category 1" },
+    { id: "2", name: "Category 2" },
+    { id: "3", name: "Category 3" },
+  ]);
+  const [newCategory, setNewCategory] = useState("");
+
+  const handleAddCategory = () => {};
+
+  return (
+    <Select onValueChange={onChangeHandler} defaultValue={value}>
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+      <SelectContent>
+        {category.length > 0 &&
+          category.map((category) => (
+            <SelectItem
+              key={category?.id}
+              value={category?.id}
+              className="select-item p-regular-14"
+            >
+              {category?.name}
+            </SelectItem>
+          ))}
+        <AlertDialog>
+          <AlertDialogTrigger className="p-medium-14 flex w-full reounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">
+            Add New Category
+          </AlertDialogTrigger>
+          <AlertDialogContent className="bg-white">
+            <AlertDialogHeader>
+              <AlertDialogTitle>New Category</AlertDialogTitle>
+              <AlertDialogDescription>
+                <Input
+                  type="text"
+                  placeholder="Category name"
+                  className="input-field mt-3"
+                  onChange={(e) => setNewCategory(e.target.value)}
+                />
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                onClick={() => startTransition(handleAddCategory)}
+              >
+                Add Category
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default Dropdown;
---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { eventFormSchema } from "@/lib/validator";
+import * as z from "zod";
+import { eventDefaultValues } from "@/constants";
+import Dropdown from "./Dropdown";
+import { Textarea } from "../ui/textarea";
+
+type EventFormProps = {
+  userId: string;
+  type: "Create" | "Update";
+};
+
+const EventForm = ({ userId, type }: EventFormProps) => {
+  const initialValues = eventDefaultValues;
+
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: initialValues,
+  });
+
+  function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-5"
+      >
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    placeholder="Event Title"
+                    {...field}
+                    className="input-field"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+           <FormField
+            control={form.control}
+            name="categoryId"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                 <Dropdown onChangeHandler={field.onChange} value={field.value}/>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="flex flex-col gap-5 md:flex-row">
+        <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl className="h-72">
+                    
+                  <Textarea
+                    placeholder="Description"
+                    {...field}
+                    className="textarea rounded-2xl"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default EventForm;
