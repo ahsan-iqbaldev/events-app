0f3a03c50e0f3657c9c268f3ba76714e9fd39ab4
Ahsan Iqbal <revensolutions01@gmail.com>
2024-06-01 17:59:47 +0500

    create navbar

---
+++ components/shared/NavItems.tsx
@@ -0,0 +1,27 @@
+"use client"
+import { headerLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const NavItems = () => {
+  const pathName = usePathname();
+  return (
+    <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
+      {headerLinks.map((link) => {
+        const isActive = pathName == link.route;
+        return (
+          <li
+            className={`${
+              isActive && "text-primary-500"
+            } flex-center p-medium-16 whitespace-nowrap`}
+          >
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default NavItems;
