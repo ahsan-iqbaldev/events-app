fd40cf14e160a9cc42f6a554a85322f10082ce68
Ahsan Iqbal <revensolutions01@gmail.com>
2024-06-01 18:27:54 +0500

    complete banner section

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+        <p>2024 Evently. All right reserved</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
