5e5a90369c7ee25f10ec7d7995cb4b15844c6841
Ahsan Iqbal <revensolutions01@gmail.com>
2024-06-07 02:56:46 +0500

    handle create event integration

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,11 +1,7 @@
 import EventForm from "@/components/shared/EventForm";
-import { auth } from "@clerk/nextjs/server";
 import React from "react";
 
 const CreateEvent = () => {
-  const { sessionClaims } = auth();
-
-  const userId = sessionClaims?.userId as string;
 
   return (
     <>
@@ -16,7 +12,7 @@ const CreateEvent = () => {
       </section>
 
       <div className="wrapper my-8">
-        <EventForm userId={userId} type="Create" />
+        <EventForm type="Create" />
       </div>
     </>
   );
