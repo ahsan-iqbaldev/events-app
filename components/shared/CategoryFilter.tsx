91daf715baa07c1e38a38821daa474e0b1e11f76
Ahsan Iqbal <revensolutions01@gmail.com>
2024-06-10 04:17:59 +0500

    handle category filteration

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -10,37 +10,37 @@ import {
 // import { getAllCategories } from "@/lib/actions/category.actions";
 // import { ICategory } from "@/lib/database/models/category.model";
 import { formUrlQuery, removeKeysFromQuery } from "@/lib/utils";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { startTransition, useEffect, useState } from "react";
+import { Input } from "../ui/input";
+import { useDispatch, useSelector } from "react-redux";
+import { ThunkDispatch } from "@reduxjs/toolkit";
+import { addCategory, getCategoryEvents } from "@/store/slices/eventSlice";
+import { toast } from "react-toastify";
 
-const CategoryFilter = () => {
-  const [categories, setCategories] = useState<any[]>([]);
+const CategoryFilter = ({ existingCategory }: any) => {
+  const dispatch = useDispatch<ThunkDispatch<any, any, any>>();
+  const { User } = useSelector((state: any) => state.auth);
+  const userId = User?.userId;
+  const [newCategory, setNewCategory] = useState("");
+  const [categories, setCategories] = useState<any[]>(existingCategory);
   const router = useRouter();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    const arr = [
-      {
-        name: "AI",
-        _id: "8718973827",
-      },
-      {
-        name: "Website develpeor",
-        _id: "92830498149",
-      },
-      {
-        name: "Computer",
-        _id: "810983099021",
-      },
-    ];
-    const getCategories = async () => {
-      //   const categoryList = await getAllCategories();
-
-      arr && setCategories(arr as any[]);
-    };
-
-    getCategories();
-  }, []);
+    setCategories(existingCategory);
+  }, [existingCategory]);
 
   const onSelectCategory = (category: string) => {
     let newUrl = "";
@@ -59,6 +59,22 @@ const CategoryFilter = () => {
     }
 
     router.push(newUrl, { scroll: false });
+    dispatch(getCategoryEvents({category}));
+  };
+
+  const handleAddCategory = () => {
+    const payload = {
+      category: [newCategory],
+      userId,
+    };
+    dispatch(
+      addCategory({
+        payload,
+        onSuccess: () => {
+          toast.success("Category Added sucessfully");
+        },
+      })
+    );
   };
 
   return (
@@ -73,13 +89,41 @@ const CategoryFilter = () => {
 
         {categories.map((category) => (
           <SelectItem
-            value={category.name}
-            key={category._id}
+            value={category}
+            key={category}
             className="select-item p-regular-14"
           >
-            {category.name}
+            {category}
           </SelectItem>
         ))}
+        <AlertDialog>
+          <AlertDialogTrigger className="p-medium-14 flex w-full reounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">
+            Add New Category
+          </AlertDialogTrigger>
+          <AlertDialogContent className="bg-white">
+            <AlertDialogHeader>
+              <AlertDialogTitle>New Category</AlertDialogTitle>
+              <AlertDialogDescription>
+                <Input
+                  type="text"
+                  placeholder="Category name"
+                  className="input-field mt-3"
+                  onChange={(e) => setNewCategory(e.target.value)}
+                />
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                onClick={() => {
+                  startTransition(handleAddCategory);
+                }}
+              >
+                Add Category
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </SelectContent>
     </Select>
   );
